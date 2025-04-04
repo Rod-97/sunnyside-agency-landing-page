f38d0c942d699ec1e7a0cdcf6297cd7f343987d6
Rodrigo <200969810+Rod-97@users.noreply.github.com>
2025-04-04 19:11:51 +0200

    navbar done

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("test");
+const hamburgerIcon = document.getElementsByClassName("hamburger-icon")[0];
+const mobileNav = document.getElementsByClassName("mobile-nav")[0];
+
+hamburgerIcon.addEventListener("click", () => {
+  mobileNav.classList.toggle("active");
+});
